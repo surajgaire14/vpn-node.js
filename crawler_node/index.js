2ed0010088a437d30a6b8788d1400704370d2957
surajgaire14 <eriag321@gmail.com>
2022-04-10 13:16:59 +0545

    Migrate Crawler to AWS

--- crawler_node/index.js
+++ crawler_node/index.js
@@ -1,27 +1,48 @@
-const cluster = require('cluster');
-// file deepcode ignore HttpToHttps: HttpToHttps in Production
-const http = require('http');
-const numCPUs = require('os').cpus().length;
-const process = require('process');
+const url = "https://porngifs.com/";
+const puppeteer = require("puppeteer");
+const https = require("https");
+// deepcode ignore HttpToHttps: ignoring es6 format
+const fs = require("fs");
 
-if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is running`);
+const download = (url, destination) =>
+  new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(destination);
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+    https
+      .get(url, (response) => {
+        response.pipe(file);
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
+        file.on("finish", () => {
+          file.close(resolve(true));
+        });
+      })
+      .on("error", (error) => {
+        fs.unlink(destination);
+
+        reject(error.message);
+      });
   });
-} else {
-  // Workers can share any TCP connection
-  // In this case it is an HTTP server
-  http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('hello world\n');
-  }).listen(8000);
 
-  console.log(`Worker ${process.pid} started`);
-}
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url, { timeout: 0 });
+  let result;
+
+  const images = await page.evaluate(() =>
+    Array.from(document.images, (e) => e.src)
+  );
+  console.log(images);
+
+  for (let i = 0; i < images.length; i++) {
+    result = await download(images[i], `/home/ubuntu/public/image-${i}.png`);
+
+    if (result === true) {
+      console.log("Success:", images[i], "has been downloaded successfully.");
+    } else {
+      console.log("Error:", images[i], "was not downloaded.");
+      console.error(result);
+    }
+  }
+  await browser.close();
+})();
